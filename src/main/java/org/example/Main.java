9ee58ff15577a4a5a5524ebfafe34a72c2e2d809
Pafer1701 <paoolo3521@gmail.com>
2025-05-07 09:52:46 -0500

    Se agrego funcionalidad

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello JavaBank!");
+        System.out.println("Hello JavaBank ATM! Version control with Git ");
     }
 }
