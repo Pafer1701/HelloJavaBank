b9d7097e8a0211872bff78bd9b07820cc39686ff
Pafer1701 <paoolo3521@gmail.com>
2025-05-07 11:36:40 -0500

    Proyecto Javabank inicial

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,21 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello JavaBank ATM! Version control with Git ");
+
+        String accountNumber = "123456789";
+        double balance =  2500.30;
+        int pin = 1234;
+
+        int[] transactionsAmounts = {400,-50,150};
+
+        balance += transactionsAmounts[0];
+        if (balance > 0 && pin == 1234){
+            System.out.println("Acceso Concedido");
+        }
+
+        balance++; // Ajusta el saldo
+        String status = (balance < 0) ? "Deuda" : "Crédito";
+        System.out.println("Estado de cuenta: " + status);
+
     }
 }
